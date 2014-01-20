3c5c4b7435bcc12b07725ad9b3707b2533c9ac78
Renan <renanccastro@gmail.com>
2014-01-20 15:54:32 -0200

    Initial commit, adding things.

---
+++ server.js
@@ -0,0 +1,41 @@
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var crypto = require('crypto');
+
+var cronJob = require('cron').CronJob;
+new cronJob('0 0-23/2 * * *', function(){
+    require("./updater.js");
+    console.log('Updating database...');
+}, null, true);
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  var queryData = url.parse(request.url, true).query;
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  var json_file = queryData.file;
+  var json_hash = queryData.hash;
+
+
+  if (json_file && json_hash) {
+      fs.readFile(json_file, function(err, fd){
+          if(!err){
+              var hash_new = crypto.createHash('md5').update(fd.toString()).digest('hex');
+              if(hash_new != json_hash){
+                  response.end(fd.toString());
+                  console.log("teste");
+              }
+              else{
+                  response.end("nil");
+              }
+          }
+          else{
+          }
+      });
+  }
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
---
+++ updater.js
@@ -0,0 +1,155 @@
+var http = require("http");
+var url = require("url");
+var get = require('get');
+var crypto = require('crypto');
+var cheerio = require('cheerio');
+var google = new google();
+var querystring = require('querystring');
+var document = new document();
+var fs = require('fs');
+var ida = new Array();
+var ida_used = 0;
+var pontos = new Array();
+var volta = new Array();
+var nomesDasLinhas = new Array();
+var codigo = new Array();
+var md5 = require('MD5');
+
+var number = 3;
+
+var asd = " ";
+    //url =   argv.x;
+//number = argv.y;
+
+
+get( ' http://www.emdec.com.br/ABusInf/consultarLinha.asp?consulta=1 ').asString(function(err, data){
+    if(err)
+        throw err;
+    $ = cheerio.load(data);
+    var b = $('.cinza a');
+    var a = $(" strong ");
+    nomesDasLinhas = a.text().split('\n');
+
+
+    console.log(b.length);
+    for(var i = 0; i < b.length ; i++){
+            codigo.push(querystring.parse(url.parse(b[i].attribs["href"]).query)["CdPjOID"]);
+    }
+    //console.log(codigo.length);
+    for(var j = 0; j < codigo.length; j++){
+        var get = require('get');
+        number = codigo[j];
+        console.log(number);
+        asd = 'http://www.emdec.com.br/ABusInf/ABInfSvItiDLGoogleM.asp?CdPjOID=' + number + '&TpDiaID=0';
+        console.log(asd);
+        setTimeout(getString(asd, number, j+1), 30000);
+    }
+});
+
+
+function getString(asd, number, number2 ){
+
+        get(asd).asString(function(err, data) {
+            if(err) throw err;
+            $ = cheerio.load(data);
+            treatArray($('script').contents().text(), number, number2);
+
+        });
+}
+
+
+
+
+
+function treatArray(script, number, number2){
+    //console.log(script);
+
+    var window = new Object();
+    eval(script);
+    initialize();
+    saveArrays(ida, volta, pontos, number, number2);
+}
+
+function saveArrays(polyline_ida, polyline_volta ,   markers, number, number2){
+    var name = nomesDasLinhas[number2];
+    var dictionary = {};
+    dictionary["name"] = name;
+    dictionary["polyline_ida"] = polyline_ida;
+    dictionary["polyline_volta"] = polyline_volta;
+    dictionary["pontos"] = markers;
+    var outputFilename = 'line_' + number2 + '.json';
+    //Verify if the file exists, create a md5 and check it
+    writeToFile(dictionary, outputFilename);
+}
+
+function writeToFile(dictionary, outputFilename){
+    fs.writeFile(outputFilename, JSON.stringify(dictionary, null, 4), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("JSON " + outputFilename + " saved. ");
+        }
+    });
+
+}
+
+function maps(){
+    this.MapTypeId = new MapTypeId();
+    this.Map = function(a){
+    };
+    this.event = new event();
+    this.Polyline = function(a){
+        return new polyline(a);
+    }
+    this.Marker = function(a){
+        b = new marker(a);
+        return b;
+    };
+    this.LatLng = function (lat, lng){
+        b = new latlang(lat, lng);
+        return b;
+    }
+}
+function google(){
+    this.maps = new maps();
+}
+function MapTypeId(){
+    this.ROADMAP = 1;
+}
+function document(){
+    this.getElementById = function (a){
+
+    }
+}
+function polyline(a){
+    this.a = a;
+    if(ida_used){
+        ida = a.path;
+    }
+    else{
+        volta = a.path;
+        ida_used = 1;
+    }
+    this.setMap = function(b){
+
+    };
+}
+
+function marker(b){
+    this.b = b;
+
+    pontos.push(b.position);
+    this.setMap = function(c){
+    };
+}
+function event(){
+    this.addListener = function(a, b, c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+function latlang(lat, lng){
+    this.lat = lat;
+    this.lng = lng;
+}
