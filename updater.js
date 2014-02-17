95ecded6cf24be09f931725e52bb693b00932655
Renan <renanccastro@gmail.com>
2014-02-17 19:40:11 -0300

    Revert "der"
    This reverts commit eb081e1bb63c9fd49fe2afe5bf11227c14ee8684.

--- updater.js
+++ updater.js
@@ -104,7 +104,7 @@ exports.updateServer = function ( response ){
                 if (error != null) {
                     console.log('exec error: ' + error);
                 }
-                response.end("Updated was needed, you now have the newest database version on the server");
+                console.log("Updated was needed, you now have the newest database version on the server");
                 fse.removeSync("json/");
                 fs.renameSync("new_json/","json/");
 
